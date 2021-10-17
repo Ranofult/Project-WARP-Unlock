ed7529a4dc2aff1ded502d41d3c120fca88d4ac1
Ranofult <50669355+Ranofult@users.noreply.github.com>
2021-10-17 16:05:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ function Check_System_Depandencies {
 }
 
 function Download_Profile {
-    wget -qO /etc/dnsmasq.d/warp.conf https://raw.githubusercontent.com/acacia233/Project-WARP-Unlock/main/dnsmasq/warp.conf
+    wget -qO /etc/dnsmasq.d/warp.conf https://raw.githubusercontent.com/Ranofult/Project-WARP-Unlock/main/dnsmasq/warp.conf
     wget -qO /etc/wireguard/up https://raw.githubusercontent.com/acacia233/Project-WARP-Unlock/main/scripts/up
     wget -qO /etc/wireguard/down https://raw.githubusercontent.com/acacia233/Project-WARP-Unlock/main/scripts/down
     chmod +x /etc/wireguard/up
